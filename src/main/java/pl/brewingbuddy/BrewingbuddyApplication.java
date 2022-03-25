0a035402cb4d0b099f8025585f083f5fb7fe073b
DHodaniewicz <d.hodaniewicz@gmail.com>
2022-03-25 21:26:38 +0100

    Initial commit

---
+++ src/main/java/pl/brewingbuddy/BrewingbuddyApplication.java
@@ -0,0 +1,13 @@
+package pl.brewingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewingbuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewingbuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/brewingbuddy/BrewingbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.brewingbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingbuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
