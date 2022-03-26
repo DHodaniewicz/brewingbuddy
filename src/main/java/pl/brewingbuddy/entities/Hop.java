7632166d7eaa277146cfc1e32c3f27b002987d80
DHodaniewicz <d.hodaniewicz@gmail.com>
2022-03-26 11:17:50 +0100

    Added Hop entity

---
+++ src/main/java/pl/brewingbuddy/entities/Hop.java
@@ -0,0 +1,13 @@
+package pl.brewingbuddy.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
